75a8009191acaa09cc8473f7d95c712ba8480c13
khoitd1997 <khoidinhtrinh@gmail.com>
2019-05-30 18:52:00 -0700

    chore: Change project name

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace hellodotnetcore
+namespace eecs113_webapp
 {
     public class Program
     {
--- Startup.cs
+++ Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace hellodotnetcore
+namespace eecs113_webapp
 {
     public class Startup
     {
