d262dd5cb0bfb8d401bc10cd4166fa072f6b1126
khoitd1997 <khoidinhtrinh@gmail.com>
2019-05-31 02:49:33 -0700

    feat: Added some basic html

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function toggleVisibility() {
+    var x = document.getElementById("target");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    alert("BLA");
+    var level = document.getElementById("navDemo").getElementById("l1");
+    level.textContent = "Goodbye world!";
+
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
