347e15e5ad2849414331ea391ea1ffb2b1169992
khoitd1997 <khoidinhtrinh@gmail.com>
2019-05-31 12:37:37 -0700

    feat: Added some styling

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,26 +1,47 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
-function toggleVisibility() {
-    var x = document.getElementById("target");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
+var utils = {
+    hasClass: function (element, className) {
+        return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
+    },
+
+    toggleVisibility: function (element) {
+        if (element.style.display === "none") {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
+        }
+    },
+
+    turnVisibilityOff: function (element) {
+        element.style.display = "none";
+    },
+
+    turnVisibilityOn: function (element) {
+        element.style.display = "block";
     }
 }
 
-// Used to toggle the menu on small screens when clicking on the menu button
-function myFunction() {
-    alert("BLA");
-    var level = document.getElementById("navDemo").getElementById("l1");
-    level.textContent = "Goodbye world!";
+// window.onload = function () {
+//     var buttons = document.getElementById('navbar').getElementsByTagName('*');
+//     for (let button of buttons) {
+//         button.onclick = changeTab(button.id);
+//     }
+// }
 
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
+// Write your JavaScript code.
+function changeTab(currButtonID) {
+    var buttons = document.getElementById('navbar').getElementsByTagName('*');
+    for (let button of buttons) {
+        if (utils.hasClass(button, "w3-white")) {
+            button.classList.remove("w3-white");
+            utils.turnVisibilityOff(document.getElementById("main_content" + button.id.match(/\d+$/)));
+        }
     }
+
+    var currButton = document.getElementById(currButtonID)
+    currButton.classList.add("w3-white");
+    utils.turnVisibilityOn(document.getElementById("main_content" + currButton.id.match(/\d+$/)));
+    // utils.toggleVisibility(document.getElementById("main_content" + currButtonID.match(/\d+$/)));
 }
